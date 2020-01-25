fc02a2fb3e143d7669ee497eb3d42b3b0a4784b5
rakesh <36419224+JavastackspaceIT@users.noreply.github.com>
2020-01-25 20:27:02 +0530

    I remove the unwanted semicolon

--- src/main/java/com/javastackspaceIT/jwt/config/SecurityConfig.java
+++ src/main/java/com/javastackspaceIT/jwt/config/SecurityConfig.java
@@ -48,6 +48,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        
+        
+        
+        
     }
 }
